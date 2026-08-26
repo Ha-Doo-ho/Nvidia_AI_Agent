1f847a4dda276dc8ae44db522557af5cebf1a517
두호 하 <whtjszjavbxj15@naver.com>
2026-08-27 01:07:22 +0900

    modified RNN_Practice01_03.py

--- RNN/DAY15/RNN_Seqence_Practice/RNN_Practice01_03.py
+++ RNN/DAY15/RNN_Seqence_Practice/RNN_Practice01_03.py
@@ -16,16 +16,19 @@ from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 As a convention, "0" does not stand for a specific word, but instead is used to encode the pad token.
 0은 패딩으로 사용한다는 것이다. RNN에서는 문장 길이를 맞추어야 하기 때문이다. 늘 강조하듯이 패딩과 마스킹은 따로 해야 한다. 
 패딩된 것은 마스킹을 시켜서 의미를 부여하지 않도록 막아야 한다. 
+
+Embedding을 사용하는 모델이라면 “Embedding이 처리할 수 있는 단어 ID 범위”는 반드시 정해야 한다.
+그래서 VOCAB_SIZE는 반드시 들어가야 한다.
 """
 
 # 1 데이터
 VOCAB_SIZE = 10000 # 사전에 등록된 단어는 몇 종류로 할 것인가? 즉 서로 다른 단어 종류를 몇 개 사용할 것인가?
 MAX_LENGTH = 200   # 문장의 길이는 얼마나 되는지. 즉 단어를 ID로 표현하는데, ID 중복 상관 없이 length=200까지 허용하겠다는 뜻
 EMBEDDING_DIM = 128   # 문장은 단어로 쪼개지고 그 단어는 컴퓨터가 못알아 먹으니까 ID로 되어 있다. 즉 단어=ID인데, 그 단어를 표현하기 위해서 몇 length의 vector로 표현할 것인가이다. 128이면 128 length의 vector로 단어 하나를 표현한다.
+PATH = "./_save/keras_RNN_imdb01.keras"
 
 
-
-(x_train, y_train), (x_test, y_test) = imdb.load_data()
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=VOCAB_SIZE)
 # print(x_train.shape, y_train.shape) #(25000,) (25000,)
 # print(x_test.shape, y_test.shape)   #(25000,) (25000,)
 # print(x_train) #1, 14, 22, 16, 43, 530, 973, 1622, 1385, 65, 458, 4468, 66, . . .단어 하나가 그에 맞는 id로 변환되어 있다. 이 id는 건들면 안된다.
@@ -45,17 +48,40 @@ x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, train_size=0
 
 # 2.모델 구성
 model = Sequential()
-model.add(Input(shape=(200, ))) # 입력 자체가 단어 ID(지금은 그 ID 종류가 200개임)가 1렬로 놓인 벡터 형태이다. 
+model.add(Input(shape=(MAX_LENGTH, ))) # 입력가 단어 ID(지금은 그 ID가 들어갈 수 있는 인덱스 개수200개)가 1렬로 놓인 벡터 형태이다. 
 model.add(Embedding(input_dim=VOCAB_SIZE, output_dim=EMBEDDING_DIM, mask_zero=True, )) #안 읽는 부분은 0으로 마스킹해서 읽으면 안된다. 
 
 model.add(GlobalAveragePooling1D())
+print(x_train.shape, x_val.shape)
 
 model.add(Dense(64, activation="relu"))
 model.add(Dropout(0.3))
 model.add(Dense(1, activation="sigmoid")) #2진분류이다. 이게 긍정인가 부정인가를 따지니까 2진분류이며 2진분류는 Sigmoid (이건 암기해야 함. 다중은 Softmax이다.)
                                           #문장가지고 출력은 긍/부 둘 중 하나니까 마지막 출력 노드의 개수는 1이다. 
+                                          #리뷰마다 긍정일 확률 하나를 출력한다. 그게 0.5 이상이면 긍정 그 미만이면 부정이라고 생각할 수 있다. 시그모이드이므로 0초과 1미만이다.
 model.summary()
 
 # 3.컴파일 및 훈련
 model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy", AUC(name="auc")])
-EarlyStopping(monitor="val_loss", )
+es = EarlyStopping(monitor="val_accuracy", patience=3, mode="max", restore_best_weights=True)
+mcp = ModelCheckpoint(filepath=PATH, monitor="val_accuracy", mode="max", save_best_only=True,)
+
+start_time = time.time()
+model.fit(x_train, y_train, batch_size=64, epochs=10 ,validation_data=(x_val, y_val), callbacks=[es, mcp], shuffle=True)
+end_time = time.time()
+
+# 4. 평가 및 예측
+loss, accuracy, auc = model.evaluate(x_test, y_test)
+y_predict = model.predict(x_test).flatten() # [ []  ] 이런 형태로 나오기 때문에 1차원으로 한번 펴야 한다. 그래서 flatten 사용하였음. 
+y_predict_probability = (y_predict >= 0.5).astype("int32") # 0.5기준으로 같거나 크면 1 작으면 0 int32 즉 정수로 변환하라는 의미이다. 이걸로 긍/부를 알아낸다.  
+print(y_predict.shape, y_predict_probability.shape)
+
+print("accuracy: ", np.round(accuracy,2), " auc: ", np.round(auc, 4))
+print(y_predict_probability[:20])
+
+cm = confusion_matrix(y_test, y_predict_probability)
+labels = ["Negative", "Positive"] #0이 부정이고 1이 긍정이므로, 순서가 바뀌면 햇갈린다. 순서를 유지시켜줄 것
+cm_display = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+cm_display.plot(cmap="Blues")
+plt.show()
+
